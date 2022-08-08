1fd1a13debe4be663ab52692e632f45b542352bb
Andrea <andrea97uni@gmail.com>
2022-08-08 14:10:03 +0200

    k8s client for custom orchestrator

--- custom-orchestrator/utils/orchestrate_k8s.py
+++ custom-orchestrator/utils/orchestrate_k8s.py
@@ -1,7 +1,13 @@
-import kubernetes
-
-
-def create_client(ip):
+from kubernetes import config, client as kclient
+def create_client(logger):
+    # client created to be executed inside a pod within the cluster
+    try:
+        # load the cluster configuration
+        config.load_incluster_config()
+        k8s_client = kclient.CoreV1Api()
+        return k8s_client
+    except kclient.exceptions.ApiException as e:
+        logger.error(e)
     return
 
 
