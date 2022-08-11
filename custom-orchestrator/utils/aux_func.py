ca5d5856f916c4338ca19b8206707ccace43cdcb
Andrea <andrea97uni@gmail.com>
2022-08-11 14:15:26 +0200

    added functions to control namespace resource limitation

--- custom-orchestrator/utils/aux_func.py
+++ custom-orchestrator/utils/aux_func.py
@@ -1,3 +1,4 @@
+import kubernetes.client as kclient
 # auxiliary functions to set nodes as tainted or untainted
 
 # taint/untaint nodes using the patch_node function
@@ -14,3 +15,16 @@ def untaint_node(kube_client, node_name):
 def scale_replicas(client, name, ns, replicaNumber):
     patch_body = {"spec": {"replicas": replicaNumber}}
     client.patch_namespaced_deployment_scale(name, ns, patch_body)
+
+
+# functions to add/remove resource quotas in a namespace
+
+def limit_node_resources(client, deployment_ns):
+    resource_quota = kclient.V1ResourceQuota(
+        spec=kclient.V1ResourceQuotaSpec(
+            hard={"limits.cpu": "700m", "limits.memory": "500M"}))
+    resource_quota.metadata = kclient.V1ObjectMeta(namespace=deployment_ns, name=f"quota-{deployment_ns}")
+    client.create_namespaced_resource_quota(deployment_ns, body=resource_quota)
+
+def delete_node_resource_limitations(client, deployment_ns):
+    client.delete_namespaced_resource_quota(name=f"quota-{deployment_ns}", namespace=deployment_ns)
